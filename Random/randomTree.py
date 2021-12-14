44bf933d9a6ccb9979981d4ce6f190c6d57db59c
weytho <thomas.weiser@skynet.be>
2021-12-14 12:43:09 +0100

    random update

--- Random/randomTree.py
+++ Random/randomTree.py
@@ -3,22 +3,18 @@ import sys
 import random
 
 # simple generation of random tree
-def nodeGeneration(Relations, CounterMeasures, Properties, node, depth, maxdepth, branching_factor, first):
+def nodeGeneration(Relations, CounterMeasures, Properties, node, depth, maxdepth, branching_factor):
 
     rng = random.randint(0,maxdepth)
     # this node is a leaf
     if rng < depth:
-        if first !=0:
-            Properties += "\n"
-        Properties += node+":{"+"cost = "+str(random.randint(1,10))+","+"prob = "+"1.0"+"}"
+        Properties += "\n"+node+":{"+"cost = "+str(random.randint(1,10))+","+"prob = "+"1.0"+"}"
         if random.randint(0,10)<3 :
             CounterMeasures += "\nCM"+node+" ("+node+")"
-        return Relations,CounterMeasures,Properties,1
-    if depth!=0:
-        Relations += "\n"
+        return Relations,CounterMeasures,Properties
     # node logic
     rnglogic = random.randint(1,3)
-    Relations += node
+    Relations += "\n"+node
     if rnglogic == 1:
         Relations += " -OR-> "
     elif rnglogic == 2:
@@ -40,34 +36,27 @@ def nodeGeneration(Relations, CounterMeasures, Properties, node, depth, maxdepth
     # node child
     if depth == maxdepth:
         for i in range(0,rngchild):
-            if first !=0:
-                Properties += "\n"
-            first = 1
-            Properties += child[i]+":{"+"cost = "+str(random.randint(1,10))+","+"prob = "+"1.0"+"}"
+            Properties += "\n"+child[i]+":{"+"cost = "+str(random.randint(1,10))+","+"prob = "+"1.0"+"}"
             if random.randint(0,10)<3 :
                 CounterMeasures += "\nCM"+child[i]+" ("+child[i]+")"
-        return Relations,CounterMeasures,Properties,1
+        return Relations,CounterMeasures,Properties
     else:
         for i in range(0,rngchild):
-            Relations,CounterMeasures,Properties,first = nodeGeneration(Relations,CounterMeasures,Properties,child[i],depth+1,maxdepth,branching_factor,first)
-    return Relations,CounterMeasures,Properties,first
+            Relations,CounterMeasures,Properties = nodeGeneration(Relations,CounterMeasures,Properties,child[i],depth+1,maxdepth,branching_factor)
+    return Relations,CounterMeasures,Properties
 
 # More complex generation with shared childrens
-def nodeGeneration2(NodeList, Relations, Properties, node, depth, maxdepth, branching_factor, first):
+def nodeGeneration2(NodeList, Relations, Properties, node, depth, maxdepth, branching_factor):
 
     rng = random.randint(0,maxdepth)
     NodeList.append(node)
     # this node is a leaf
     if rng < depth:
-        if first !=0:
-            Properties += "\n"
-        Properties += node+":{"+"cost = "+str(random.randint(1,10))+","+"prob = "+"1.0"+"}"
-        return Relations,Properties,1
-    if depth!=0:
-        Relations += "\n"
+        Properties += "\n"+node+":{"+"cost = "+str(random.randint(1,10))+","+"prob = "+"1.0"+"}"
+        return Relations,Properties
     # node logic
     rnglogic = random.randint(1,3)
-    Relations += node
+    Relations += "\n"+node
     if rnglogic == 1:
         Relations += " -OR-> "
     elif rnglogic == 2:
@@ -99,17 +88,14 @@ def nodeGeneration2(NodeList, Relations, Properties, node, depth, maxdepth, bran
     if depth == maxdepth:
         for i in range(0,rngchild):
             NodeList.append(child[i])
-            if first !=0:
-                Properties += "\n"
-            first = 1
-            Properties += child[i]+":{"+"cost = "+str(random.randint(1,10))+","+"prob = "+"1.0"+"}"
-        return Relations,Properties,1
+            Properties += "\n"+child[i]+":{"+"cost = "+str(random.randint(1,10))+","+"prob = "+"1.0"+"}"
+        return Relations,Properties
     else:
         for i in range(0,rngchild):
-            Relations,Properties,first = nodeGeneration2(NodeList, Relations,Properties,child[i],depth+1,maxdepth,branching_factor,first)
-    return Relations,Properties,first
+            Relations,Properties = nodeGeneration2(NodeList, Relations,Properties,child[i],depth+1,maxdepth,branching_factor)
+    return Relations,Properties
 
-def CMGeneration(NodeList, CounterMeasures):
+def CMGeneration2(NodeList, CounterMeasures):
     l = len(NodeList)
     if l == 0:
         return CounterMeasures
@@ -117,9 +103,71 @@ def CMGeneration(NodeList, CounterMeasures):
         CounterMeasures += "\nCM1"+" ("+NodeList[0]+")"
         return CounterMeasures
     if l == 2:
-        CounterMeasures += "\nCM1"+" ("+NodeList[0]+","+NodeList[0]+")"
+        CounterMeasures += "\nCM1"+" ("+NodeList[0]+","+NodeList[1]+")"
         return CounterMeasures
     nbrCM = round(l/3)
+    #print(nbrCM)
+    for i in range(1,nbrCM+1):
+        CounterMeasures += "\nCM"+str(i)+" ("+random.choice(NodeList)+","+random.choice(NodeList)+")"
+    return CounterMeasures
+
+
+# probably inconsistent tree
+def nodeGeneration3(NodeList, Relations, Properties, node, depth, maxdepth, branching_factor):
+
+    rng = random.randint(0,maxdepth)
+    NodeList.append(node)
+    # this node is a leaf
+    if rng < depth:
+        Properties += "\n"+node+":{"+"cost = "+str(random.randint(1,10))+","+"prob = "+"1.0"+"}"
+        return Relations,Properties
+    # node logic
+    rnglogic = random.randint(1,3)
+    Relations += "\n"+node
+    if rnglogic == 1:
+        Relations += " -OR-> "
+    elif rnglogic == 2:
+        Relations += " -AND-> "
+    else:
+        Relations += " -SAND-> "
+    # node child
+    rngchild = random.randint(2,branching_factor)
+    Relations += "{"
+    child = [node+str(i) for i in range(0,rngchild)]
+    for i in range(0,rngchild):
+        Relations += child[i]
+        if i!=rngchild-1:
+            Relations += ","
+    # Random shared children
+    if random.randint(0,10)<2:
+        #print("proba shared node! ")
+        #print(NodeList)
+        existingnode = random.choice(NodeList)
+        Relations += ","+existingnode
+    Relations += "}"
+    # node child
+    if depth == maxdepth:
+        for i in range(0,rngchild):
+            NodeList.append(child[i])
+            Properties += "\n"+child[i]+":{"+"cost = "+str(random.randint(1,10))+","+"prob = "+"1.0"+"}"
+        return Relations,Properties
+    else:
+        for i in range(0,rngchild):
+            Relations,Properties = nodeGeneration3(NodeList, Relations,Properties,child[i],depth+1,maxdepth,branching_factor)
+    return Relations,Properties
+
+def CMGeneration3(NodeList, CounterMeasures):
+    l = len(NodeList)
+    if l == 0:
+        return CounterMeasures
+    if l == 1:
+        CounterMeasures += "\nCM1"+" ("+NodeList[0]+")"
+        return CounterMeasures
+    if l == 2:
+        CounterMeasures += "\nCM1"+" ("+NodeList[0]+","+NodeList[1]+")"
+        return CounterMeasures
+    nbrCM = round(l/3)
+    #print(nbrCM)
     for i in range(1,nbrCM+1):
         node1 = random.choice(NodeList)
         node2 = random.choice(NodeList)
@@ -128,23 +176,25 @@ def CMGeneration(NodeList, CounterMeasures):
         CounterMeasures += "\nCM"+str(i)+" ("+node1+","+node2+")"
     return CounterMeasures
 
-
-
 # complexity 1 : relations + Countermeasures + Properties
 # complexity 2 : shared child + shared CM
-# complexity 3 : TBD
+# complexity 3 : probably inconsistent tree
 def TreeGen(maxdepth, branching_factor, complexity):
     maxdepth = max(1,maxdepth)
     branching_factor = max(2,branching_factor)
-    Relations = """RELATIONS\n"""
+    Relations = """RELATIONS"""
     CounterMeasures = """COUNTERMEASURES"""
-    Properties = """PROPERTIES\n"""
+    Properties = """PROPERTIES"""
     if complexity == 1:
-        Relations,CounterMeasures,Properties,first = nodeGeneration(Relations,CounterMeasures,Properties,"node",0,maxdepth, branching_factor,0)
+        Relations,CounterMeasures,Properties = nodeGeneration(Relations,CounterMeasures,Properties,"node",0,maxdepth, branching_factor)
     if complexity == 2:
         NodeList = []
-        Relations,NodeList,Properties,first = nodeGeneration2(Relations,CounterMeasures,Properties,"node",0,maxdepth, branching_factor,0)
-        CounterMeasures = CMGeneration(NodeList,CounterMeasures)
+        Relations,Properties = nodeGeneration2(NodeList,Relations,Properties,"node",0,maxdepth, branching_factor)
+        CounterMeasures = CMGeneration2(NodeList,CounterMeasures)
+    if complexity == 3:
+        NodeList = []
+        Relations,Properties = nodeGeneration3(NodeList,Relations,Properties,"node",0,maxdepth, branching_factor)
+        CounterMeasures = CMGeneration3(NodeList,CounterMeasures)
     return Relations,CounterMeasures,Properties
 
 maxdepth = 3
@@ -154,15 +204,20 @@ if len(sys.argv)>=3:
         branching_factor = int(sys.argv[2])
 maxdepth = max(1,maxdepth)
 branching_factor = max(2,branching_factor)   
-Relations = """RELATIONS\n"""
+Relations = """RELATIONS"""
 CounterMeasures = """COUNTERMEASURES"""
-Properties = """PROPERTIES\n"""
+Properties = """PROPERTIES"""
 NodeList = []
-#Relations,CounterMeasures,Properties,first = nodeGeneration(Relations,CounterMeasures,Properties,"node",0,maxdepth, branching_factor,0)
+Relations,CounterMeasures,Properties = nodeGeneration(Relations,CounterMeasures,Properties,"node",0,maxdepth, branching_factor)
 
-Relations,Properties,first = nodeGeneration2(NodeList,Relations,Properties,"node",0,maxdepth, branching_factor,0)
-CounterMeasures = CMGeneration(NodeList,CounterMeasures)
+#Relations,Properties,first = nodeGeneration3(NodeList,Relations,Properties,"node",0,maxdepth, branching_factor,0)
+#CounterMeasures = CMGeneration3(NodeList,CounterMeasures)
 
-print(Relations)
-print(CounterMeasures)
-print(Properties)
+#print(Relations)
+#print(CounterMeasures)
+#print(Properties)
+
+rel,cm,prop = TreeGen(2,3,3)
+print(rel)
+print(cm)
+print(prop)
