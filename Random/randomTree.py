58099d13144a1b27976f38bfcf7393567486916e
wey <thomas.weiser@skynet.be>
2022-01-20 15:48:05 +0100

    updaterandom solved bug

--- Random/randomTree.py
+++ Random/randomTree.py
@@ -101,15 +101,12 @@ def nodeGeneration2(NodeList, Relations, Properties, node, depth, maxdepth, bran
             Relations += ","
     # Random shared children
     if random.randint(0,10)<8:
-        #print("proba shared node! ")
-        #print(NodeList)
         existingnode = random.choice(NodeList)
         cnt = 5
         while(existingnode in node and cnt > 0):
             existingnode = random.choice(NodeList)
             cnt-=1
         if cnt > 0:
-            #print("Shared node! ")
             Relations += ","+existingnode
     Relations += "}"
     # node child
@@ -137,14 +134,27 @@ def CMGeneration2(NodeList, CounterMeasures):
         CounterMeasures += "\nCM1"+" ("+NodeList[0]+")"
         return CounterMeasures
     if l == 2:
-        CounterMeasures += "\nCM1"+" ("+NodeList[0]+","+NodeList[1]+")"
+        r = random.randint(0,1)
+        if r==0:
+            CounterMeasures += "\nCM1"+" ("+NodeList[1]+")"
+        else:
+            CounterMeasures += "\nCM1"+" ("+NodeList[0]+","+NodeList[1]+")"
         return CounterMeasures
     nbrCM = round(l/3)
-    #print(nbrCM)
     for i in range(1,nbrCM+1):
-        CounterMeasures += "\nCM"+str(i)+" ("+random.choice(NodeList)+","+random.choice(NodeList)+")"
+        r = random.randint(0,1)
+        if r==0:
+            CounterMeasures += "\nCM"+str(i)+" ("+random.choice(NodeList)+")"
+        elif r==1:
+            n1 = random.choice(NodeList)
+            n2 = random.choice(NodeList)
+            cnt = 10
+            while(n2==n1 and cnt>0):
+                n2 = random.choice(NodeList)
+                cnt-=1
+            if cnt > 0:
+                CounterMeasures += "\nCM"+str(i)+" ("+n1+","+n2+")"
     return CounterMeasures
-
 
 ##
 # recursive tree generator 3
@@ -188,8 +198,6 @@ def nodeGeneration3(NodeList, Relations, Properties, node, depth, maxdepth, bran
             Relations += ","
     # Random shared children
     if random.randint(0,10)<2:
-        #print("proba shared node! ")
-        #print(NodeList)
         existingnode = random.choice(NodeList)
         Relations += ","+existingnode
     Relations += "}"
@@ -221,13 +229,12 @@ def CMGeneration3(NodeList, CounterMeasures):
         CounterMeasures += "\nCM1"+" ("+NodeList[0]+","+NodeList[1]+")"
         return CounterMeasures
     nbrCM = round(l/3)
-    #print(nbrCM)
     for i in range(1,nbrCM+1):
-        node1 = random.choice(NodeList)
-        node2 = random.choice(NodeList)
-        while node2 == node1:
-            node2 = random.choice(NodeList)
-        CounterMeasures += "\nCM"+str(i)+" ("+node1+","+node2+")"
+        r = random.randint(0,1)
+        if r==0:
+            CounterMeasures += "\nCM"+str(i)+" ("+random.choice(NodeList)+")"
+        else:
+            CounterMeasures += "\nCM"+str(i)+" ("+random.choice(NodeList)+","+random.choice(NodeList)+")"
     return CounterMeasures
 
 ##
@@ -246,37 +253,19 @@ def TreeGen(maxdepth, branching_factor, complexity):
     Properties = """PROPERTIES"""
     if complexity == 1:
         Relations,CounterMeasures,Properties = nodeGeneration(Relations,CounterMeasures,Properties,"node",0,maxdepth, branching_factor)
-    if complexity == 2:
+    elif complexity == 2:
         NodeList = []
         Relations,Properties = nodeGeneration2(NodeList,Relations,Properties,"node",0,maxdepth, branching_factor)
         CounterMeasures = CMGeneration2(NodeList,CounterMeasures)
-    if complexity == 3:
+    elif complexity == 3:
         NodeList = []
         Relations,Properties = nodeGeneration3(NodeList,Relations,Properties,"node",0,maxdepth, branching_factor)
         CounterMeasures = CMGeneration3(NodeList,CounterMeasures)
+    else:
+        print("Wrong complexity : [0:3]")
     return Relations,CounterMeasures,Properties
 
-maxdepth = 3
-branching_factor = 2
-if len(sys.argv)>=3:
-        maxdepth = int(sys.argv[1])
-        branching_factor = int(sys.argv[2])
-maxdepth = max(1,maxdepth)
-branching_factor = max(2,branching_factor)   
-Relations = """RELATIONS"""
-CounterMeasures = """COUNTERMEASURES"""
-Properties = """PROPERTIES"""
-NodeList = []
-Relations,CounterMeasures,Properties = nodeGeneration(Relations,CounterMeasures,Properties,"node",0,maxdepth, branching_factor)
-
-#Relations,Properties,first = nodeGeneration3(NodeList,Relations,Properties,"node",0,maxdepth, branching_factor,0)
-#CounterMeasures = CMGeneration3(NodeList,CounterMeasures)
-
-#print(Relations)
-#print(CounterMeasures)
-#print(Properties)
-
-rel,cm,prop = TreeGen(2,3,3)
+rel,cm,prop = TreeGen(3,3,2)
 print(rel)
 print(cm)
 print(prop)
