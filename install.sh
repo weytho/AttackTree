67f7225ec4394cc46fcf844b93c20066f409ba37
delcourtfl <37302870+delcourtfl@users.noreply.github.com>
2022-05-15 01:19:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,10 @@ echo ${FULLDIR} > ${FULLFILE}
 sudo apt-get update  # To get the latest package lists
 sudo apt-get install libjson-c-dev -y
 
+sudo apt-get install python3-dev
 sudo apt install python3-pip
+sudo apt-get install graphviz graphviz-dev
+
 pip install --upgrade pip
 pip install -r requirements.txt
 pip install .
