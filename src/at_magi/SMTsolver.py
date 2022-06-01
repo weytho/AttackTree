fad0302c30879ad36b93212f92249e39734611d6
delcourtfl <37302870+delcourtfl@users.noreply.github.com>
2022-06-01 18:19:10 +0200

    Fix SMT solver xor logic

--- src/at_magi/SMTsolver.py
+++ src/at_magi/SMTsolver.py
@@ -4,10 +4,11 @@
 #
 from pysmt.shortcuts import Solver, Symbol, Real, Plus, And, Equals, Ite, Times, GT, LT, GE, LE
 from pysmt.typing import REAL
-from pysmt.parsing import parse
+from pysmt.parsing import parse, HRParser, InfixOpAdapter
 import z3
 from fractions import Fraction
 from pysmt.oracles import get_logic
+from collections import namedtuple
 
 # ! Instead of using Forall to compare all existing models to find the minimum -> Minimize -> Simplex Z3
 
@@ -17,10 +18,10 @@ def SMTformating(solutions, list_var):
     for l in solutions:
         tmp = [-1]*len(list_var)
         for e in l:
-            if z3.is_true(l[e]):
-                tmp[list_var.index(str(e()))] = 1
-            elif e.name() == "%SOL%":
-                values_array.append(l[e])
+            if z3.is_true(e[1]):
+                tmp[list_var.index(str(e[0]))] = 1
+            elif str(e[0]) == "%SOL%":
+                values_array.append(e[1])
         bool_only_list.append(tmp)
 
     return list_var, bool_only_list, values_array
@@ -36,7 +37,14 @@ def SMTcost(list_var, list_cost, formula, upper_bound=None):
     SOL = Symbol("%SOL%", REAL)
     list_cond_cost = [Ite(s, c, zero) for s, c in zip(list_symbols, list_smt_cost)]
 
-    problem = parse(formula)
+    HR = HRParser()
+    Rule = namedtuple('Rule', ['regex', 'symbol', 'is_functional'])
+    for r in HR.lexer.rules:
+        if r[0] == r"(\^)":
+            HR.lexer.rules[HR.lexer.rules.index(r)] = Rule(r"(\^)", InfixOpAdapter(HR.lexer.mgr.Xor, 10), False)
+            break
+    problem = HR.parse(formula)
+
     sum = Plus(list_cond_cost)
 
     if upper_bound != None:
@@ -79,18 +87,21 @@ def SMTcost(list_var, list_cost, formula, upper_bound=None):
             elif not get_all and z3.simplify(new_best > best):
                 break
 
-            solutions.append(model)
             block = []
+            filtered_model = []
             for d in model:
                 c = d()
-                block.append(c != model[d])
+                if str(c) in list_var or str(c) == "%SOL%":
+                    filtered_model.append((c, model[d]))
+                    block.append(c != model[d])
+
+            solutions.append(filtered_model)
             o.add(z3.Or(block))
 
         vars, sols, values = SMTformating(solutions, list_var)
         return vars, sols, best, values
 
 def SMTproba(list_var, list_proba, formula, lower_bound=0):
-
     formula = formula.replace('~', '!')
     list_symbols = [Symbol(s) for s in list_var]
     list_smt_proba = [Real(p) for p in list_proba]
@@ -102,7 +113,14 @@ def SMTproba(list_var, list_proba, formula, lower_bound=0):
     SOL = Symbol("%SOL%", REAL)
     list_and_proba = [Ite(s, p, one) for s, p in zip(list_symbols, list_smt_proba)]
 
-    problem = parse(formula)
+    HR = HRParser()
+    Rule = namedtuple('Rule', ['regex', 'symbol', 'is_functional'])
+    for r in HR.lexer.rules:
+        if r[0] == r"(\^)":
+            HR.lexer.rules[HR.lexer.rules.index(r)] = Rule(r"(\^)", InfixOpAdapter(HR.lexer.mgr.Xor, 10), False)
+            break
+    problem = HR.parse(formula)
+
     prod = Times(list_and_proba)
 
     if lower_bound > 0:
@@ -146,11 +164,15 @@ def SMTproba(list_var, list_proba, formula, lower_bound=0):
                 # Get all min solutions with the same proba
                 break
 
-            solutions.append(model)
             block = []
+            filtered_model = []
             for d in model:
                 c = d()
-                block.append(c != model[d])
+                if str(c) in list_var or str(c) == "%SOL%":
+                    filtered_model.append((c, model[d]))
+                    block.append(c != model[d])
+
+            solutions.append(filtered_model)
             o.add(z3.Or(block))
 
         vars, sols, values = SMTformating(solutions, list_var)
@@ -165,11 +187,12 @@ if __name__ == "__main__":
     list_cost = [3, 1, 2, 2]
     list_cost = [Fraction(str(x)) for x in list_cost]
     
-    list_proba = [0.7, 0.2, 0.5, 0.5]
+    list_proba = [0.7, 0.66, 0.5, 0.5]
     list_proba = [Fraction(str(x)) for x in list_proba]
 
-    formula = " (X1 | X2) & (X3 | ~ X4) "
+    formula = " (X1 | X2) & (X3 ^ X4) "
     #formula = " (X1 | X2)"
+    #formula = " (X1 ^ X2 ^ X3 ^ X4) "
 
     cost_max = Fraction(str(6))
     proba_min = Fraction(str(0.3))
@@ -177,13 +200,13 @@ if __name__ == "__main__":
     #z3.set_option(verbose=10)
 
     print("COST")
-    #print(SMTcost(list_var, list_cost, formula, cost_max))
+    print(SMTcost(list_var, list_cost, formula, cost_max))
 
     print()
     print(SMTcost(list_var, list_cost, formula))
 
     print("PROBA")
-    #print(SMTproba(list_var, list_proba, formula, proba_min))
+    print(SMTproba(list_var, list_proba, formula, proba_min))
 
     print()
     print(SMTproba(list_var, list_proba, formula))
